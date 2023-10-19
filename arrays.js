923bb679d4b12dff5e0031f61c63cdd3410465b8
IsabelIMTextil <imtextil@gmail.com>
2023-10-19 12:53:06 -0300

    correción de Daniel

--- arrays.js
+++ arrays.js
@@ -3,9 +3,9 @@ alert("En la dietética ´Tu salud más saludable´ podemos ofrecerte estas gall
 var productos = ["1. Bread Net", "2. Cereal Mix", "3. Frutigran", "4. Nutresan Algarroba y avena", "5. Granix salvado", "6. María E. Algarroba", "7. María E. Avena", "8. Pepas La Francia"]
 var precios = [950, 500, 650, 1050, 456, 2870, 2870, 1145]
 var indice = 0
-totalProductos = precios * unidades
+//total = precios * unidades
 
-console.log(productos.length)
+//console.log(productos.length)
 
 while(indice<productos.length){
     console.log("Producto " + productos[indice] + ". " + " Precio: $ " + precios[indice])
@@ -14,15 +14,15 @@ while(indice<productos.length){
 }
 
 
-var seleccion = prompt("Seleccioná un producto, introduciendo su número (1-" + productos.length +"):")   
+var seleccion = parseInt(prompt("Seleccioná un producto, introduciendo su número (1-" + productos.length +"):"))   
 
 var unidades = parseInt(prompt("Cuántas unidades quiere?"))
 
-//var precioTotal = unidades * precios[indice]
+var total = parseInt(unidades) * precios[parseInt(seleccion)-1]
 
-alert("Total a abonar $" + " " + totalProductos)
+alert("Total a abonar $" + " " + total)
 
-console.log("Total a abonar $" + " " + totalProductos)
+console.log("Total a abonar $" + " " + total)
 
 
 
